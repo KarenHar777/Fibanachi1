fc5f0cd4ffea966271c97b7535248801d00e2218
KarenHarutyunyan <karenvardanovich@gmail.com>
2022-12-03 01:07:11 +0400

    I do it

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,23 +4,28 @@ namespace ConsoleApp2
 {
     class Program
     {
-        static void Fibanachi(int x, int y, int z)
+        static void Fibanachi(int n,int x, int y, int z)
         {
-            
-            for (int i = 0; i < 10; i++)
+
+            for (int i = 0; i < 1; i++)
             {
                 z = x + y;
                 x = y;
                 y = z;
-                Console.WriteLine(z);
+                Console.WriteLine($"{n} number - " + z);
             }
-            return;
-            Fibanachi(x, y, z);
+            
+            n++;
+            if (n==11)
+            {
+                return;
+            }
+            Fibanachi(n, x, y, z);
         }
-        
+
         static void Main(string[] args)
         {
-            Fibanachi(1, 0, 0);
+            Fibanachi(1,1, 0, 0);
             Console.WriteLine();
             Console.WriteLine("I do It");
             Console.ReadKey();
