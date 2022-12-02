a2a18018ede0d10823ec34ee4ae1c869210a4eca
KarenHarutyunyan <karenvardanovich@gmail.com>
2022-12-03 00:44:36 +0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Fibanachi(int x, int y, int z)
+        {
+            
+            for (int i = 0; i < 10; i++)
+            {
+                z = x + y;
+                x = y;
+                y = z;
+                Console.WriteLine(z);
+            }
+            return;
+            Fibanachi(x, y, z);
+        }
+        
+        static void Main(string[] args)
+        {
+            Fibanachi(1, 0, 0);
+            Console.WriteLine();
+            Console.WriteLine("I do It");
+            Console.ReadKey();
+        }
+    }
+}
